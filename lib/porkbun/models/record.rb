d5d5591cbc085e6e80df2dc2aa9f9ef5cb5d134a
Dean Perry <dean@deanpcmad.com>
2024-06-01 19:23:36 +0100

    more work, added records

--- lib/porkbun/models/record.rb
+++ lib/porkbun/models/record.rb
@@ -1,4 +1,55 @@
 module Porkbun
   class Record < Object
+
+    class << self
+
+      def list(domain:)
+        response = Client.post_request("dns/retrieve/#{domain}", body: {})
+        Collection.from_response(response, type: Record, key: "records")
+      end
+
+      def list_by_type(domain:, type:, subdomain:)
+        response = Client.post_request("dns/retrieveByNameType/#{domain}/#{type}/#{subdomain}", body: {})
+        Collection.from_response(response, type: Record, key: "records")
+      end
+
+      def retrieve(domain:, id:)
+        response = Client.post_request("dns/retrieve/#{domain}/#{id}", body: {})
+        Record.new(response.body["records"][0])
+      end
+
+      def create(domain:, type:, content:, **options)
+        body = {
+          type: type,
+          content: content,
+        }.merge(options)
+        response = Client.post_request("dns/create/#{domain}", body: body)
+        Record.new(id: response.body["id"]) if response.body["status"] == "SUCCESS"
+      end
+
+      def update(domain:, id:, type:, content:, **options)
+        body = {
+          type: type,
+          content: content,
+        }.merge(options)
+        response = Client.post_request("dns/edit/#{domain}/#{id}", body: body)
+        response.body["status"] == "SUCCESS"
+      end
+
+      def update_by_subdomain(domain:, type:, subdomain:, content:, **options)
+        body = {
+          content: content,
+        }.merge(options)
+        response = Client.post_request("dns/editByNameType/#{domain}/#{type}/#{subdomain}", body: body)
+        response.body["status"] == "SUCCESS"
+      end
+
+      def delete_by_subdomain(domain:, type:, subdomain:)
+        response = Client.post_request("dns/deleteByNameType/#{domain}/#{type}/#{subdomain}", body: {})
+        response.body["status"] == "SUCCESS"
+      end
+
+    end
+
   end
 end
---
+++ test/porkbun/models/record_test.rb
@@ -0,0 +1,65 @@
+require "test_helper"
+
+class RecordTest < Minitest::Test
+
+  def test_record_list
+    records = Porkbun::Record.list(domain: "deanpcmad.dev")
+    assert_equal Porkbun::Collection, records.class
+    assert_equal Porkbun::Record, records.data.first.class
+    assert_equal "pixie.porkbun.com", records.data.first.content
+  end
+
+  def test_record_list_by_type
+    records = Porkbun::Record.list_by_type(domain: "deanpcmad.dev", type: "CNAME", subdomain: "*")
+    assert_equal Porkbun::Collection, records.class
+    assert_equal Porkbun::Record, records.data.first.class
+    assert_equal "pixie.porkbun.com", records.data.first.content
+  end
+
+  def test_record_retrieve
+    record = Porkbun::Record.retrieve(domain: "deanpcmad.dev", id: "253421437")
+    assert_equal Porkbun::Record, record.class
+    assert_equal "pixie.porkbun.com", record.content
+  end
+
+  def test_record_create
+    record = Porkbun::Record.create(
+      domain: "deanpcmad.dev",
+      type: "A",
+      content: "1.2.3.4",
+      name: "test2"
+    )
+    assert_equal Porkbun::Record, record.class
+  end
+
+  def test_record_update
+    record = Porkbun::Record.update(
+      domain: "deanpcmad.dev",
+      id: "404108078",
+      type: "A",
+      content: "1.1.1.1",
+      name: "test2"
+    )
+    assert record
+  end
+
+  def test_record_update_by_subdomain
+    record = Porkbun::Record.update_by_subdomain(
+      domain: "deanpcmad.dev",
+      type: "A",
+      subdomain: "test2",
+      content: "2.2.2.2"
+    )
+    assert record
+  end
+
+  def test_record_delete_by_subdomain
+    record = Porkbun::Record.delete_by_subdomain(
+      domain: "deanpcmad.dev",
+      type: "A",
+      subdomain: "test2"
+    )
+    assert record
+  end
+
+end
